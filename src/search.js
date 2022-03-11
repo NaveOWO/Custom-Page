b8726773ea090ca8dbd9fec24d633a5190b7b94b
NaveOWO <pianowell96@gmail.com>
2022-03-12 04:01:28 +0900

    script code

---
+++ src/search.js
@@ -0,0 +1,10 @@
+// ver1
+// const query = document.querySelector('.query');
+// const searchBtn = document.querySelector('.material-icons');
+// searchBtn.onclick = function() {
+//   const url = 'https://www.google.com/search?q='+query.value;
+//   window.open(url);
+// }
+
+// ver2
+
