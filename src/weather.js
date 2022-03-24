23554999b6026290e8f9e3deb85212341a26f656
NaveOWO <pianowell96@gmail.com>
2022-03-24 18:11:11 +0900

    날씨제공 기능구현

--- src/weather.js
+++ src/weather.js
@@ -1,43 +1,8 @@
-const COORDS = "coords";
-const WEATHER_KEY = "5bc237fd1f9840c10e6355ebdc9aef89";
+const apiId = "5bc237fd1f9840c10e6355ebdc9aef89";
 
-function createWeather(weather) {
-  const textTemp = document.querySelector(".header__weather--temperature");
-  const textLoaction = document.querySelector(".header__weather--location");
-  const weatherIcon = document.querySelector(".header__weather--ico");
-  const weatherContainer = document.querySelector(
-    ".header__weather--container"
-  );
-  const temp = weather.main.temp;
-  const name = weather.name;
+navigator.geolocation.getCurrentPosition(success, error);
 
-  textTemp.innerText = `${temp}°`;
-  textLoaction.innerText = name;
-}
-
-function getWeather(coords) {
-  const lat = coords.latitude;
-  const lon = coords.longitude;
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_KEY}`,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((json) => createWeather(json))
-    .catch((error) => console.log("error", error));
-  console.log("ok");
-}
-
-function saveCoords(coordsobj) {
-  localStorage.setItem(COORDS, JSON.stringify(coordsobj));
-}
-
-function succCoordsHandle(position) {
+function success(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   const coordsObj = {
@@ -45,26 +10,26 @@ function succCoordsHandle(position) {
     longitude,
   };
   getWeather(coordsObj);
-  saveCoords(coordsObj);
 }
 
-function errLoacitonHandle() {
-  console.log("cant access geo location");
+function error(error) {
+  console.log(error);
 }
 
-function loadCoords() {
-  navigator.geolocation.getCurrentPosition(succCoordsHandle, errLoacitonHandle);
+function getWeather(coordsObj) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coordsObj.latitude}&lon=${coordsObj.longitude}&appid=${apiId}`
+  )
+    .then((response) => response.json())
+    .then((json) => createWeather(json))
+    .catch((error) => console.log(error));
 }
 
-function init() {
-  const coords = localStorage.getItem(COORDS);
-  if (coords === null) {
-    loadCoords();
-  } else {
-    const coordsParse = JSON.parse(coords);
-    getWeather(coordsParse);
-    setInterval(() => getWeather(coordsParse), 60000);
-  }
+function createWeather(weather) {
+  const textTemp = document.querySelector(".header__weather--temperature");
+  const textName = document.querySelector(".header__weather--location");
+  const temp = weather.main.temp;
+  const name = weather.name;
+  textTemp.innerText = `${temp}℃`;
+  textName.innerHTML = name;
 }
-
-init();
